fef1f2b7b9a028335ee63642d010a794bef57ffb
Olumide Falomo <olumightyfalomo@gmail.com>
2020-10-22 23:54:30 +0100

    Install global npm package(s)

--- fresh.sh
+++ fresh.sh
@@ -2,15 +2,20 @@
 
 echo "Setting up your Mac..."
 
+echo "Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Update Homebrew recipes
+echo "Brew Update"
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
+echo "Brew Bundle"
 brew tap homebrew/bundle
 brew bundle
 
@@ -18,9 +23,11 @@ brew bundle
 # mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install global Composer packages
+echo "Composer install: valet, laravel"
 /usr/local/bin/composer global require laravel/installer laravel/valet
 
 # Install Laravel Valet
+echo "Valet install"
 $HOME/.composer/vendor/bin/valet install
 
 # Create a Sites directory
@@ -34,7 +41,12 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | zsh
 
+# npm install typescript -g
+# npm install yarn -g
+# npm install serverless -g
+
 # Symlink the Mackup config file to the home directory
+echo "Symlink Mackup config"
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences
